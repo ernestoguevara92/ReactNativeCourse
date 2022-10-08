922df49b17d301f3bca651101f00390f8894a1f4
Roberto Guevara <robertoeguevara@gmail.com>
2022-10-08 14:39:14 -0700

    Layouts and Flexbox

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Hello World!</Text>
-      </View>
-      <Text style={styles.dummyText}>Learning React</Text>
-      <Button title='Tap me!' />
+    <View style={styles.appContainer}>
+     <View>
+      <TextInput placeholder='Your course goal!'/>
+      <Button title='ADD GOAL'/>
+     </View>
+     <View>
+      <Text>List of goals...</Text>
+     </View>
     </View>
   );
 } 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: 'blue',
+ appContainer: {
+  padding: 50
   }
 });
