e5e1e4382f298713c99bc9a9ed1f5853e2547220
Roberto Guevara <robertoeguevara@gmail.com>
2022-10-07 19:11:01 -0700

    exploring basic components

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello!</Text>
+      <View>
+        <Text>World!</Text>
+      </View>
+      <Button title='Tap me!' />
     </View>
   );
 }
