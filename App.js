64e127309214d3b54f475fe98ceed51619e152db
Roberto Guevara <robertoeguevara@gmail.com>
2022-10-14 21:49:00 -0700

    style the input container and textInput

--- App.js
+++ App.js
@@ -43,22 +43,6 @@ const styles = StyleSheet.create({
     paddingTop: 60,
     paddingHorizontal: 16
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8
-  },
   goalsContainer: {
     flex: 6,
   }
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -17,3 +17,22 @@ function GoalInput() {
 };
 
 export default GoalInput;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc'
+      },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        width: '70%',
+        marginRight: 8,
+        padding: 8
+      }
+});
