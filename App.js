d29c967d32451cdfd0d11ccc7eda913f92ee4587
Roberto Guevara <robertoeguevara@gmail.com>
2022-10-15 13:33:13 -0700

    pass the id as props to GoalItem Pressable for deletion

--- App.js
+++ App.js
@@ -29,7 +29,10 @@ export default function App() {
      <View style={styles.goalsContainer}>
       <FlatList data={courseGoals} 
         renderItem={(itemData) => {
-          return <GoalItem text={itemData.item.text} onDeleteItem={deleteGoalHandler} />;
+          return <GoalItem 
+            text={itemData.item.text} 
+            id={itemData.item.id}
+            onDeleteItem={deleteGoalHandler} />;
         }}
         keyExtractor={(item, index) => {
           return item.id;
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,7 +2,7 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 function GoalItem(props) {
     return (
-        <Pressable onPress={props.onDeleteItem}>
+        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
             <View style={styles.goalItem}>
                 <Text style={styles.goalItemText}>{props.text}</Text>
             </View>
